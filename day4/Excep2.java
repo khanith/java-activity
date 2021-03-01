24415bb3c1b149535ff30bef5cf167326f906e59
99002636 <aditi.dutta@ltts.com>
2021-03-01 17:15:31 +0530

    excep

---
+++ day4/Excep2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Excep2{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the number of overs: ");
+		try
+		{
+			int size = Integer.parseInt(reader.readLine());
+			int array[] = new int[size];
+			for(int i=0;i<size;i++)
+			{
+				array[i] = Integer.parseInt(reader.readLine());
+				
+			}
+			System.out.println("Over Number: ");
+			int num = Integer.parseInt(reader.readLine());
+			System.out.println(array[num-1]);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
